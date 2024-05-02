d8fcd598a3c124c42fc8c60b3f2ec4ae174fa5a0
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2024-05-02 09:45:56 +0100

    Update playRound function to display correct game prompts and scores

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ const heading = document.createElement("h1");
 heading.textContent = "Rock Paper Scissors Game";
 introDiv.appendChild(heading);
 
+let round = 1;
+
 const introPara = document.createElement("p");
-introPara.textContent = "Click any of the buttons to make your choice";
+introPara.textContent = `Round ${round}. Click any of the buttons to make your choice.`;
 introDiv.appendChild(introPara);
 
 const rockBtn = document.createElement("button");
@@ -33,99 +35,142 @@ const getComputerChoice = () => {
 };
 
 const playRound = () => {
+  let playerScore = 0;
+  let computerScore = 0;
+  let result = "";
   let resultPara = "";
 
   rockBtn.addEventListener("click", (e) => {
     const computerChoice = getComputerChoice();
 
     if (computerChoice === "scissors") {
-      resultPara += `<p>You chose: ${e.target.textContent.toLowerCase()}<br>Computer chose: 
+      resultPara = `<p>You chose: ${e.target.textContent.toLowerCase()}<br>Computer chose: 
         ${computerChoice}<br>You win round! ${e.target.textContent.toLowerCase()} beats ${computerChoice}<br></p>`;
+      playerScore++;
+      result = `<p>Your score: ${playerScore}<br>Computer score: ${computerScore}</p><br>`;
     } else if (computerChoice === "paper") {
-      resultPara += `<p>You chose: ${e.target.textContent.toLowerCase()}<br>Computer chose: 
+      resultPara = `<p>You chose: ${e.target.textContent.toLowerCase()}<br>Computer chose: 
         ${computerChoice}<br>You lose round! ${computerChoice} beats ${e.target.textContent.toLowerCase()}<br></p>`;
+      computerScore++;
+      result = `<p>Your score: ${playerScore}<br>Computer score: ${computerScore}</p><br>`;
     } else {
-      resultPara += `<p>You chose: ${e.target.textContent.toLowerCase()}<br>Computer chose: 
+      resultPara = `<p>You chose: ${e.target.textContent.toLowerCase()}<br>Computer chose: 
         ${computerChoice}<br>Round is a tie! You both chose ${computerChoice}<br></p>`;
+      result = `<p>Your score: ${playerScore}<br>Computer score: ${computerScore}</p><br>`;
     }
 
-    resultDiv.innerHTML = resultPara;
+    resultDiv.innerHTML = resultPara + result;
     body.appendChild(resultDiv);
 
-    return resultPara;
+    if (round < 5) {
+      round++;
+      introPara.textContent = `Round ${round}. Click any of the buttons to make your choice.`;
+    } else {
+      round = 1;
+      if (playerScore > computerScore) {
+        resultDiv.innerHTML = "";
+        resultDiv.innerHTML = result + "<br>YOU WIN!!!";
+      } else if (playerScore < computerScore) {
+        resultDiv.innerHTML = "";
+        resultDiv.innerHTML = result + "<br>YOU LOSE!!!";
+      } else {
+        resultDiv.innerHTML = "";
+        resultDiv.innerHTML = result + "<br>GAME IS A TIE!!!";
+      }
+      playerScore = 0;
+      computerScore = 0;
+      introPara.textContent = "";
+      introPara.innerHTML = `PLAY AGAIN<br>Round ${round}. Click any of the buttons to make your choice.`;
+    }
   });
 
   paperBtn.addEventListener("click", (e) => {
     const computerChoice = getComputerChoice();
 
     if (computerChoice === "rock") {
-      resultPara += `<p>You chose: ${e.target.textContent.toLowerCase()}<br>Computer chose: 
+      resultPara = `<p>You chose: ${e.target.textContent.toLowerCase()}<br>Computer chose: 
         ${computerChoice}<br>You win round! ${e.target.textContent.toLowerCase()} beats ${computerChoice}<br></p>`;
+      playerScore++;
+      result = `<p>Your score: ${playerScore}<br>Computer score: ${computerScore}</p><br>`;
     } else if (computerChoice === "scissors") {
-      resultPara += `<p>You chose: ${e.target.textContent.toLowerCase()}<br>Computer chose: 
+      resultPara = `<p>You chose: ${e.target.textContent.toLowerCase()}<br>Computer chose: 
         ${computerChoice}<br>You lose round! ${computerChoice} beats ${e.target.textContent.toLowerCase()}<br></p>`;
+      computerScore++;
+      result = `<p>Your score: ${playerScore}<br>Computer score: ${computerScore}</p><br>`;
     } else {
-      resultPara += `<p>You chose: ${e.target.textContent.toLowerCase()}<br>Computer chose: 
+      resultPara = `<p>You chose: ${e.target.textContent.toLowerCase()}<br>Computer chose: 
         ${computerChoice}<br>Round is a tie! You both chose ${computerChoice}<br></p>`;
+      result = `<p>Your score: ${playerScore}<br>Computer score: ${computerScore}</p><br>`;
     }
 
-    resultDiv.innerHTML = resultPara;
+    resultDiv.innerHTML = resultPara + result;
     body.appendChild(resultDiv);
 
-    return resultPara;
+    if (round < 5) {
+      round++;
+      introPara.textContent = `Round ${round}. Click any of the buttons to make your choice.`;
+    } else {
+      round = 1;
+      if (playerScore > computerScore) {
+        resultDiv.innerHTML = "";
+        resultDiv.innerHTML = result + "<br>YOU WIN!!!";
+      } else if (playerScore < computerScore) {
+        resultDiv.innerHTML = "";
+        resultDiv.innerHTML = result + "<br>YOU LOSE!!!";
+      } else {
+        resultDiv.innerHTML = "";
+        resultDiv.innerHTML = result + "<br>GAME IS A TIE!!!";
+      }
+      playerScore = 0;
+      computerScore = 0;
+      introPara.textContent = "";
+      introPara.innerHTML = `PLAY AGAIN<br>Round ${round}. Click any of the buttons to make your choice.`;
+    }
   });
 
   scissorsBtn.addEventListener("click", (e) => {
     const computerChoice = getComputerChoice();
 
     if (computerChoice === "paper") {
-      resultPara += `<p>You chose: ${e.target.textContent.toLowerCase()}<br>Computer chose: 
+      resultPara = `<p>You chose: ${e.target.textContent.toLowerCase()}<br>Computer chose: 
         ${computerChoice}<br>You win round! ${e.target.textContent.toLowerCase()} beats ${computerChoice}<br></p>`;
+      playerScore++;
+      result = `<p>Your score: ${playerScore}<br>Computer score: ${computerScore}</p><br>`;
     } else if (computerChoice === "rock") {
-      resultPara += `<p>You chose: ${e.target.textContent.toLowerCase()}<br>Computer chose: 
+      resultPara = `<p>You chose: ${e.target.textContent.toLowerCase()}<br>Computer chose: 
         ${computerChoice}<br>You lose round! ${computerChoice} beats ${e.target.textContent.toLowerCase()}<br></p>`;
+      computerScore++;
+      result = `<p>Your score: ${playerScore}<br>Computer score: ${computerScore}</p><br>`;
     } else {
-      resultPara += `<p>You chose: ${e.target.textContent.toLowerCase()}<br>Computer chose: 
+      resultPara = `<p>You chose: ${e.target.textContent.toLowerCase()}<br>Computer chose: 
         ${computerChoice}<br>Round is a tie! You both chose ${computerChoice}<br></p>`;
+      result = `<p>Your score: ${playerScore}<br>Computer score: ${computerScore}</p><br>`;
     }
 
-    resultDiv.innerHTML = resultPara;
+    resultDiv.innerHTML = resultPara + result;
     body.appendChild(resultDiv);
 
-    return resultPara;
+    if (round < 5) {
+      round++;
+      introPara.textContent = `Round ${round}. Click any of the buttons to make your choice.`;
+    } else {
+      round = 1;
+      if (playerScore > computerScore) {
+        resultDiv.innerHTML = "";
+        resultDiv.innerHTML = result + "<br>YOU WIN!!!";
+      } else if (playerScore < computerScore) {
+        resultDiv.innerHTML = "";
+        resultDiv.innerHTML = result + "<br>YOU LOSE!!!";
+      } else {
+        resultDiv.innerHTML = "";
+        resultDiv.innerHTML = result + "<br>GAME IS A TIE!!!";
+      }
+      playerScore = 0;
+      computerScore = 0;
+      introPara.textContent = "";
+      introPara.innerHTML = `PLAY AGAIN<br>Round ${round}. Click any of the buttons to make your choice.`;
+    }
   });
-};
-
-const playGame = () => {
-  let playerScore = 0;
-  let computerScore = 0;
-  let playerSelection;
-  let computerSelection;
-  let roundResult;
-
-  // for (let i = 0; i < 5; i++) {
-  // playerSelection = prompt(
-  ("Choose between 'rock', 'paper' and 'scissors':");
-  // ).toLowerCase();
-  computerSelection = getComputerChoice();
-  roundResult = playRound(playerSelection, computerSelection);
-
-  if (roundResult === "win") {
-    playerScore++;
-  } else if (roundResult === "lose") {
-    computerScore++;
-    // } else {
-    //   continue;
-    // }
-  }
-
-  if (playerScore === computerScore) {
-    return `Your score: ${playerScore}\nComputer score: ${computerScore}\n\nGAME IS A TIE!!!`;
-  } else if (playerScore > computerScore) {
-    return `Your score: ${playerScore}\nComputer score: ${computerScore}\n\nYOU WIN!!!`;
-  } else {
-    return `Your score: ${playerScore}\nComputer score: ${computerScore}\n\nYOU LOSE!!!`;
-  }
 };
 
 playRound();
