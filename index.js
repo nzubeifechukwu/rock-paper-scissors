8aace81447e16af49d5b0a5bfe1d6498a08e6c24
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2024-05-06 10:33:22 +0100

    Add styles

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ introDiv.appendChild(heading);
 let round = 1;
 
 const introPara = document.createElement("p");
-introPara.textContent = `Round ${round}. Click any of the buttons to make your choice.`;
+introPara.innerHTML = `<strong>ROUND ${round}</strong><br>Click any of the buttons to make your choice.`;
 introDiv.appendChild(introPara);
 
 const btnContents = ["Rock", "Paper", "Scissors"];
@@ -41,51 +41,51 @@ const playRound = () => {
       const playerChoice = e.target.textContent.toLowerCase();
       if (playerChoice === "rock") {
         if (computerChoice === "scissors") {
-          resultPara = `<p>You chose: ${playerChoice}<br>Computer chose:
+          resultPara = `<p>You chose ${playerChoice}<br>Computer chose
         ${computerChoice}<br>You win round! ${playerChoice} beats ${computerChoice}</p>`;
           playerScore++;
-          result = `<p>Your score: ${playerScore}<br>Computer score: ${computerScore}</p>`;
+          result = `<strong><p>Your score: ${playerScore}<br>Computer score: ${computerScore}</p></strong>`;
         } else if (computerChoice === "paper") {
-          resultPara = `<p>You chose: ${playerChoice}<br>Computer chose:
+          resultPara = `<p>You chose ${playerChoice}<br>Computer chose
         ${computerChoice}<br>You lose round! ${computerChoice} beats ${playerChoice}</p>`;
           computerScore++;
-          result = `<p>Your score: ${playerScore}<br>Computer score: ${computerScore}</p>`;
+          result = `<strong><p>Your score: ${playerScore}<br>Computer score: ${computerScore}</p></strong>`;
         } else {
-          resultPara = `<p>You chose: ${playerChoice}<br>Computer chose:
+          resultPara = `<p>You chose ${playerChoice}<br>Computer chose
         ${computerChoice}<br>Round is a tie! You both chose ${computerChoice}</p>`;
-          result = `<p>Your score: ${playerScore}<br>Computer score: ${computerScore}</p>`;
+          result = `<strong><p>Your score: ${playerScore}<br>Computer score: ${computerScore}</p></strong>`;
         }
       } else if (playerChoice === "paper") {
         if (computerChoice === "rock") {
-          resultPara = `<p>You chose: ${playerChoice}<br>Computer chose:
+          resultPara = `<p>You chose ${playerChoice}<br>Computer chose
         ${computerChoice}<br>You win round! ${playerChoice} beats ${computerChoice}</p>`;
           playerScore++;
-          result = `<p>Your score: ${playerScore}<br>Computer score: ${computerScore}</p>`;
+          result = `<strong><p>Your score: ${playerScore}<br>Computer score: ${computerScore}</p></strong>`;
         } else if (computerChoice === "scissors") {
-          resultPara = `<p>You chose: ${playerChoice}<br>Computer chose:
+          resultPara = `<p>You chose ${playerChoice}<br>Computer chose
         ${computerChoice}<br>You lose round! ${computerChoice} beats ${playerChoice}</p>`;
           computerScore++;
-          result = `<p>Your score: ${playerScore}<br>Computer score: ${computerScore}</p>`;
+          result = `<strong><p>Your score: ${playerScore}<br>Computer score: ${computerScore}</p></strong>`;
         } else {
-          resultPara = `<p>You chose: ${playerChoice}<br>Computer chose:
+          resultPara = `<p>You chose ${playerChoice}<br>Computer chose
         ${computerChoice}<br>Round is a tie! You both chose ${computerChoice}</p>`;
-          result = `<p>Your score: ${playerScore}<br>Computer score: ${computerScore}</p>`;
+          result = `<strong><p>Your score: ${playerScore}<br>Computer score: ${computerScore}</p></strong>`;
         }
       } else {
         if (computerChoice === "paper") {
-          resultPara = `<p>You chose: ${playerChoice}<br>Computer chose:
+          resultPara = `<p>You chose ${playerChoice}<br>Computer chose
         ${computerChoice}<br>You win round! ${playerChoice} beats ${computerChoice}</p>`;
           playerScore++;
-          result = `<p>Your score: ${playerScore}<br>Computer score: ${computerScore}</p>`;
+          result = `<strong><p>Your score: ${playerScore}<br>Computer score: ${computerScore}</p></strong>`;
         } else if (computerChoice === "rock") {
-          resultPara = `<p>You chose: ${playerChoice}<br>Computer chose:
+          resultPara = `<p>You chose ${playerChoice}<br>Computer chose
         ${computerChoice}<br>You lose round! ${computerChoice} beats ${playerChoice}</p>`;
           computerScore++;
-          result = `<p>Your score: ${playerScore}<br>Computer score: ${computerScore}</p>`;
+          result = `<strong><p>Your score: ${playerScore}<br>Computer score: ${computerScore}</p></strong>`;
         } else {
-          resultPara = `<p>You chose: ${playerChoice}<br>Computer chose:
+          resultPara = `<p>You chose ${playerChoice}<br>Computer chose
         ${computerChoice}<br>Round is a tie! You both chose ${computerChoice}</p>`;
-          result = `<p>Your score: ${playerScore}<br>Computer score: ${computerScore}</p>`;
+          result = `<strong><p>Your score: ${playerScore}<br>Computer score: ${computerScore}</p></strong>`;
         }
       }
 
@@ -94,19 +94,22 @@ const playRound = () => {
 
       if (round < 5) {
         round++;
-        introPara.textContent = `Round ${round}. Click any of the buttons to make your choice.`;
+        introPara.innerHTML = `<strong>ROUND ${round}</strong><br>Click any of the buttons to make your choice.`;
       } else {
         round = 1;
         if (playerScore > computerScore) {
-          resultDiv.innerHTML = resultPara + result + "<p>YOU WIN!!!</p>";
+          resultDiv.innerHTML =
+            resultPara + result + "<p><strong>YOU WIN!!!</strong></p>";
         } else if (playerScore < computerScore) {
-          resultDiv.innerHTML = resultPara + result + "<p>YOU LOSE!!!</p>";
+          resultDiv.innerHTML =
+            resultPara + result + "<p><strong>YOU LOSE!!!</strong></p>";
         } else {
-          resultDiv.innerHTML = resultPara + result + "<p>GAME IS A TIE!!!</p>";
+          resultDiv.innerHTML =
+            resultPara + result + "<p><strong>GAME IS A TIE!!!</strong></p>";
         }
         playerScore = 0;
         computerScore = 0;
-        introPara.innerHTML = `PLAY AGAIN<br>Round ${round}. Click any of the buttons to make your choice.`;
+        introPara.innerHTML = `<strong>PLAY AGAIN<br><br>ROUND ${round}</strong><br>Click any of the buttons to make your choice.`;
       }
     });
   });
